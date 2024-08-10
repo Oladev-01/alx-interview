25853a6f5b4ae37bfa12ebba153742259e0e1298
olalekan <lekanmojibola@gmail.com>
2024-08-10 10:54:16 +0100

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -23,15 +23,15 @@ def pascal_triangle(n):
     there row in pascal triangle begins from 0
     if n == 1, i return a list with an element of 1 cos in the
     pascal triangle, there is only 1 in the first row"""
-    if n <= 0:
-        return []
-
     pascal_triangle_list = []
+    if n <= 0:
+        return[[]]
+
     for row in range(n):
         # row serves as each row in the triangle
         nrow_list = []  # creating a new list for each row
         for col in range(row+1):
-            # col serves as the col for each row
+            # col serves as the column for each row
             nrow_list.append(comb(row, col))
         pascal_triangle_list.append(nrow_list)
     return pascal_triangle_list
