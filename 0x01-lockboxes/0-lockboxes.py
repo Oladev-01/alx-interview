90f08ecce2ac81bd4d758506d7005c33c702e0db
olalekan <lekanmojibola@gmail.com>
2024-08-17 18:05:24 +0100

    unlocking boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""opening boxes"""
+from typing import List
+
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """checking if all boxes can be unlocked"""
+    unlocked = [False] * len(boxes)  # starting with all the
+    # boxes not yet unlocked
+    unlocked[0] = True  # by default, first box. i.e box 0 is iunlocked
+    keys = [0]  # we only have the key to box 0, and it is unlocked
+
+    while keys:
+        box = keys.pop()  # checking the
+        # content of the unlocked box for keys to other boxes
+        for key in boxes[box]:  # checking through the unlocked box
+            if not unlocked[key]:  # if the box of that key
+                # is not yet unlocked, we unlock it
+                unlocked[key] = True  # unlocking...
+                keys.append(key)  # we add the gotten key to the list of keys
+            print(keys)
+    return all(unlocked)
