38afc66952af99f8924951d2026efd9adf7ef622
olalekan <lekanmojibola@gmail.com>
2024-08-29 11:38:39 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,6 @@
 import sys
 import signal
 
-
 if __name__ == '__main__':
     total_file_size = 0
     line_count = 0
@@ -20,34 +19,33 @@ if __name__ == '__main__':
     }
 
     def print_stats():
-        """printing the status codes"""
-        print(f"File size: {total_file_size}")
+        """printing the status codes and total file size"""
+        print(f"Total file size: {total_file_size}")
         for code in sorted(get_status_code.keys()):
             if get_status_code[code] > 0:
                 print(f"{code}: {get_status_code[code]}")
 
-    def quit_ops():
-        """handle signal interuption"""
+    def quit_ops(sig, frame):
+        """handle signal interruption"""
         print_stats()
         sys.exit(0)
 
     signal.signal(signal.SIGINT, quit_ops)
 
-    with open(sys.stdin.fileno(), 'r') as f:
-        for line in f:
-            line_count += 1
-            sp_log = line.split()
-            if len(sp_log) < 7:
-                continue
-            try:
-                status_code = int(sp_log[-2])
-                file_size = int(sp_log[-1])
-            except (ValueError, IndexError):
-                continue
-            if status_code in get_status_code:
-                get_status_code[status_code] += 1
-            total_file_size += file_size
-            if line_count % 10 == 0:
-                print_stats()
+    for line in sys.stdin:
+        line_count += 1
+        sp_log = line.split()
+        if len(sp_log) < 7:
+            continue
+        try:
+            status_code = int(sp_log[-2])
+            file_size = int(sp_log[-1])
+        except (ValueError, IndexError):
+            continue
+        if status_code in get_status_code:
+            get_status_code[status_code] += 1
+        total_file_size += file_size
+        if line_count % 10 == 0:
+            print_stats()
 
     print_stats()
