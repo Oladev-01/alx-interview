3526324be187c4fd47f27064cb4a70fa383424f5
olalekan <lekanmojibola@gmail.com>
2024-08-29 13:56:31 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,56 +1,51 @@
-#!/usr/bin/env python3
-"""log parsing"""
+#!/usr/bin/python3
+"""processing logs"""
+
 import sys
 import signal
 
-total_file_size = 0
-status_code_count = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
 
-def print_statistics():
-    """Prints the current statistics."""
-    print(f"Total file size: {total_file_size}")
-    for code in sorted(status_code_count.keys()):
-        if status_code_count[code] > 0:
-            print(f"{code}: {status_code_count[code]}")
-
-def signal_handler(sig, frame):
-    """Handles the keyboard interruption (CTRL + C)"""
-    print_statistics()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-try:
+if __name__ == '__main__':
+    total_file_size = 0
     line_count = 0
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
-        if len(parts) < 7:
-            continue
-        
-        try:
-            ip = parts[0]
-            date = parts[3][1:]
-            status_code = int(parts[-2])
-            file_size = int(parts[-1])
-        except (ValueError, IndexError):
-            continue
+    get_status_code = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0,
+    }
 
-        if status_code in status_code_count:
-            status_code_count[status_code] += 1
-        total_file_size += file_size
+    def print_stats():
+        """printing the status codes"""
+        print(f"File size: {total_file_size}")
+        for code in sorted(get_status_code.keys()):
+            if get_status_code[code] > 0:
+                print(f"{code}: {get_status_code[code]}")
 
-        if line_count % 10 == 0:
-            print_statistics()
+    def quit_ops(sig, frame):
+        """handle signal interuption"""
+        sys.exit(0)
 
-except Exception as e:
-    sys.stderr.write(f"Error: {e}\n")
+    signal.signal(signal.SIGINT, quit_ops)
+
+    with open(sys.stdin.fileno(), 'r') as f:
+        for line in f:
+            line_count += 1
+            sp_log = line.split()
+            if len(sp_log) < 7:
+                continue
+            try:
+                status_code = int(sp_log[-2])
+                file_size = int(sp_log[-1])
+            except (ValueError, IndexError):
+                continue
+            if status_code in get_status_code:
+                get_status_code[status_code] += 1
+            total_file_size += file_size
+            if line_count % 10 == 0:
+                print_stats()
+    print_stats()
